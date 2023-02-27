7ce5bdb3532d2442cfa74c919a32603b911cfc99
Harehliad Mikhail <harehliadml@gmail.com>
2023-02-27 16:22:29 +0300

    docs: add js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
+let minporog;
+let maxporog;
+let resultat; 
 class GuessingGame {
-    constructor() {}
+    
+    constructor(min, max, result) {
+        this.minporog  = min;
+        this.maxporog  = max;
+
+        
+    }
 
     setRange(min, max) {
-
+         minporog = min;
+         maxporog = max;
     }
 
     guess() {
-
+        resultat = maxporog - Math.floor((maxporog-minporog)/2);
+        return resultat;
     }
 
     lower() {
-
+        maxporog = resultat;
     }
 
     greater() {
-
+        minporog = resultat;
     }
 }
 
